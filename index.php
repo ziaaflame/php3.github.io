4c40c0c6ddaac35c50ac57ec6274503bd2d3b18e
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-24 22:40:14 +0600

    php3

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//    $name = 'Md. Zia Uddin';
+//    echo $name . '<br/>';
+//
+//    $age = '23 Years';
+//    echo $age . '<br/>';
+//
+//    $address = 'West Mekhal, Hathazari, Chittagong.';
+//    echo $address;
+
+    define('NAME','Md. Zia Uddin');
+    echo NAME . '<br/>';
+    
+    define('AGE','23 Years Old');
+    echo AGE . '<br/>';
+
+    define('FAMILY','6 Member in our Family');
+    echo FAMILY;
+?>
